683c8ad7ec6189d99ffd620637dfe171f3ce1aa8
Osybear <osybeargames@gmail.com>
2018-05-05 09:27:49 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D m_RigidBody;
+	private SpriteRenderer m_Renderer;
+	private Animator m_Animator;
+
+	public float m_MovementSpeed = 0;
+	public float m_JumpForce = 0;
+	public bool m_isGrounded = true;
+
+	private void Awake() {
+		m_RigidBody = GetComponent<Rigidbody2D>();
+		m_Renderer = GetComponent<SpriteRenderer>();
+		m_Animator = GetComponent<Animator>();
+	}
+
+	private void Update() {
+
+		float moveHorizontal = Input.GetAxisRaw("Horizontal");
+		Vector2 movement = new Vector2(moveHorizontal * m_MovementSpeed, m_RigidBody.velocity.y);
+		m_RigidBody.velocity = movement;
+
+		if(Input.GetKeyDown(KeyCode.Space) && m_isGrounded == true)
+			m_RigidBody.velocity = new Vector2(movement.x, m_JumpForce);
+
+		if(moveHorizontal < 0)
+			m_Renderer.flipX = true;
+		if(moveHorizontal > 0)
+			m_Renderer.flipX = false;
+
+		m_Animator.SetFloat("YVelocity", m_RigidBody.velocity.y);
+		m_Animator.SetInteger("Run", Mathf.Abs((int)moveHorizontal));
+	}
+
+	private void OnCollisionExit2D(Collision2D other) {
+		m_isGrounded = false;
+	}
+
+	private void OnCollisionStay2D(Collision2D other) {
+		m_isGrounded = true;
+	}
+}
